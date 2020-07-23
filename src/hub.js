e6cb06b27bf6dac854d2c74d7abd67b1efad2169
neutronstarer <neutronstarer@gmail.com>
2020-07-24 03:32:22 +0800

    no message

--- src/hub.js
+++ src/hub.js
@@ -1,30 +1,18 @@
-;
-(function () {
-  const name = '<name>'
-  const hash = require('md5')(name)
-  const hubKey = 'hub_' + hash
+;(function () {
+  const name = 'local.' + '<name>'
+  const hubKey = 'hub.' + name
   let hub = window[hubKey]
   if (hub != null) return hub
   hub = {}
   window[hubKey] = hub
-  // this buffer cache messages waiting for query
-  const messagesBuffer = []
+  // cache messages waiting for query
+  const messages = []
   const sendToNative = (function () {
     try {
-      if (window[hash].postMessage != null) { // android WebView
+      if (window[name].postMessage != null) { // android WebView
         return function (message) {
           try {
-            window[hash].postMessage(JSON.stringify(message))
-            return true
-          } catch (e) {
-            return false
-          }
-        }
-      }
-      if (window.webkit.messageHandlers[hash].postMessage != null) { // WKWebView
-        return function (message) {
-          try {
-            window.webkit.messageHandlers[hash].postMessage(JSON.stringify(message))
+            window[name].postMessage(JSON.stringify(message))
             return true
           } catch (e) {
             return false
@@ -32,39 +20,53 @@
         }
       }
     } catch (e) {
-      return function (message) {
-        // UIWebView WebView
-        try {
-          messagesBuffer.push(JSON.stringify(message))
-          const iframe = document.createElement('iframe')
-          iframe.style.display = 'none'
-          iframe.src = 'https://' + hash + '/query'
-          document.documentElement.appendChild(iframe)
-          setTimeout(function () {
-            document.documentElement.removeChild(iframe)
-          }, 1)
-          return true
-        } catch (e) {
-          return false
+      try {
+        if (window.webkit.messageHandlers[name].postMessage != null) { // WKWebView
+          return function (message) {
+            try {
+              window.webkit.messageHandlers[name].postMessage(JSON.stringify(message))
+              return true
+            } catch (e) {
+              return false
+            }
+          }
+        }
+      } catch (e) {
+        const hash = require('./lib/md5')(name)
+        return function (message) {
+          // UIWebView WebView
+          try {
+            messages.push(JSON.stringify(message))
+            const iframe = document.createElement('iframe')
+            iframe.style.display = 'none'
+            iframe.src = 'https://' + hash + '/query'
+            document.documentElement.appendChild(iframe)
+            setTimeout(function () {
+              document.documentElement.removeChild(iframe)
+            }, 1)
+            return true
+          } catch (e) {
+            return false
+          }
         }
       }
     }
   })()
   const winds = {}
-  const sendToClient = (message) => {
+  const sendToClient = function (message) {
     const { to } = message
     const wind = winds[to]
     if (wind == null) {
       return
     }
     const data = {}
-    data[hash] = message
+    data[name] = message
     wind.postMessage(data, '*')
   }
   // called by native
   hub.query = function () {
-    const str = JSON.stringify(messagesBuffer)
-    messagesBuffer.splice(0, messagesBuffer.length)
+    const str = JSON.stringify(messages)
+    messages.splice(0, messages.length)
     return str
   }
   // called by native
@@ -74,7 +76,7 @@
   }
   window.addEventListener('message', function ({ source, data }) {
     try {
-      const message = data[hash]
+      const message = data[name]
       if (message == null) {
         return
       }
@@ -84,22 +86,20 @@
       }
       if (type === 'disconnect') {
         delete winds[from]
-      }
-      if (type === 'connect') {
-        const wind = winds[from]
-        if (wind == null) {
-          winds[from] = source
+      } else {
+        if (winds[from] == null) {
+          if (type === 'connect') {
+            winds[from] = source
+          } else {
+            throw new Error('unknown window')
+          }
         }
-      }
-      if (winds[from] == null) {
-        throw new Error('unknown window')
       }
       sendToNative(message)
     } catch (e) {
       console.log(e)
     }
   })
-
   // hub did load
   const hubDidLoad = function () {
     // broadcast message to nested windows
@@ -112,10 +112,9 @@
     }
     const message = { type: 'load' }
     const data = {}
-    data[hash] = message
+    data[name] = message
     broadcast(window, data)
   }
-
   hubDidLoad()
   return hub
 })()
---
+++ src/lib/guid.js
@@ -0,0 +1,20 @@
+/* global define */
+/* eslint-disable strict */
+;(function ($) {
+  'use strict'
+  const guid = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0; var v = c === 'x' ? r : (r & 0x3 | 0x8)
+      return v.toString(16)
+    })
+  }
+  if (typeof define === 'function' && define.amd) {
+    define(function () {
+      return guid
+    })
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = guid
+  } else {
+    $.guid = guid
+  }
+})(this)
